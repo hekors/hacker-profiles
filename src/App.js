0dd9165bd113937be51a98590ab7f63b5fa9c990
Yash Sehgal <yashsehgal@Yashs-MacBook-Air.local>
2022-10-15 00:45:09 +0530

    feat: card demo added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
+import { useRef } from "react"
+import HackerProfileData from './dataSource/hacker-profiles.json';
+import Card from './component/Card';
 
 export default function App() {
+  const hackerProfileRef = useRef(HackerProfileData);
   return (
-    <div className="app w-[100vw] h-[100vh] flex flex-row items-center justify-center bg-gray-800 cursor-default">
-      <div className="app-content-container">
-        <h1 className="leading-snug text-2xl text-white font-semibold">
-          You're using a react+tailwindcss tempate
-        </h1>
-        <h3 className="leading-snug text-gray-500 font-normal">To create a react app using tailwindcss, remove the complete JSX from src/App.js</h3>
-        <div className="mt-6" />
-        <h4 className="leading-snug text-white font-semibold text-lg">
-          Extra packages this template has already installed
-        </h4>
-        <div className="leading-snug w-fit h-fit gap-2 font-medium font-mono text-purple-700 cursor-default">
-          react-router-dom, react-modal, react-icons, tailwindcss, react-tooltip
-        </div>
-        <div className="mt-12 font-normal">
-          <p className="leading-snug text-gray-500 text-sm">
-            Facing issues {"&"} problems with this template, report <a className="leading-snug text-blue-600 hover:underline" target="_blank" href="https://github.com/yashsehgal/react-tailwind-template/issues">here</a>
-          </p>
-          <p className="mt-2 leading-snug text-sm text-white">
-            Made in love with purple, react and tailwindcss by <a href="https://yashsehgal.com" target="_blank" className="leading-snug text-blue-600 hover:underline">yashsehgal</a>
-          </p>
-        </div>
-        <button className="mt-6 px-4 py-2 rounded-md bg-purple-600 text-white font-semibold flex flex-row items-center justify-center leading-snug lowercase hover:bg-blue-600 shadow-sm shadow-black"
-          onClick={() => window.open('https://tailwindcss.com/')}
-        >
-          learn tailwindcss
-        </button>
+    <div className="app">
+      <h1 className="leading-snug text-2xl text-center font-semibold mt-16">Hacker Profiles</h1>
+      <p className="leading-snug text-center text-base font-normal text-gray-400 mt-3">
+        We are happy to have your hacker profile, check out {" "}
+        <a 
+          href="https://hekors.tech" 
+          className="text-blue-500"
+          target="_blank" 
+          rel="noreferrer">
+            {"hekors.tech"}
+        </a>
+      </p>
+
+      <div className="hacker-cards-wrapper mt-24 grid grid-cols-3 gap-8 mx-auto w-fit h-auto">
+        {hackerProfileRef.current?.map((hackerProfile, hackerProfileIndex) => (
+          <Card key={hackerProfileIndex} 
+            cardData={hackerProfile} />
+        ))}
       </div>
     </div>
   )
---
+++ src/component/Card.jsx
@@ -0,0 +1,21 @@
+import { FaGithub, FaTwitter } from 'react-icons/fa';
+
+export default function Card({ cardData }) {
+    return (
+        <div className="card w-fit h-auto p-6 rounded-md bg-gray-100">
+            <div className="flex flex-col items-start justify-start gap-2">
+                <img src={`https://github.com/${cardData?.contacts?.github}.png`} className="w-24 h-24 rounded-full" />
+                <h1 className="text-base text-gray-800 font-semibold mt-3">{cardData?.firstName + " " + cardData?.lastName}</h1>
+                <span className="social-links-wrapper text-lg flex flex-row items-center justify-start gap-3 mt-2">
+                    <a href={`https://github.com/${cardData?.contacts?.github}`} target="_blank" rel='noreferrer' className='text-gray-700'>
+                        <FaGithub />
+                    </a>
+                    <a href={`https://twitter.com/${cardData?.contacts?.twitter}`} target="_blank" rel='noreferrer' className='text-blue-400'>
+                        <FaTwitter />
+                    </a>
+                </span>
+            </div>
+            <p className="leading-snug text-gray-500 mt-3 font-normal text-sm italic">{cardData?.tagline}</p>
+        </div>
+    )
+}
